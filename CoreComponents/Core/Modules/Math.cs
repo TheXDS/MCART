9fb00b270379392eb463c86e13d6983d66800ec5
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2017-11-17 23:34:20 -0600

    Funciones de Tween

--- CoreComponents/Core/Modules/Math.cs
+++ CoreComponents/Core/Modules/Math.cs
@@ -99,6 +99,60 @@ namespace MCART
         }
 
         /// <summary>
+        /// Fórmulas de suavizado.
+        /// </summary>
+        public static class Tween
+        {
+            /// <summary>
+            /// Realiza un suavizado lineal de un valor.
+            /// </summary>
+            /// <returns>
+            /// Un valor correspondiente al suavizado aplicado.
+            /// </returns>
+            /// <param name="step">Número de paso a suavizar.</param>
+            /// <param name="total">Total de pasos.</param>
+            public static float Linear(int step, int total) => (float)step / total;
+            /// <summary>
+            /// Realiza un suavizado cuadrático de un valor.
+            /// </summary>
+            /// <returns>
+            /// Un valor correspondiente al suavizado aplicado.
+            /// </returns>
+            /// <param name="step">Número de paso a suavizar.</param>
+            /// <param name="total">Total de pasos.</param>
+            public static float Quadratic(int step, int total)
+            {
+                float t = ((float)step / total);
+                return (t * t) / (2 * t * t - 2 * t + 1);
+            }
+            /// <summary>
+            /// Realiza un suavizado cúbico de un valor.
+            /// </summary>
+            /// <returns>
+            /// Un valor correspondiente al suavizado aplicado.
+            /// </returns>
+            /// <param name="step">Número de paso a suavizar.</param>
+            /// <param name="total">Total de pasos.</param>
+            public static float Cubic(int step, int total)
+            {
+                float t = ((float)step / total);
+                return (t * t * t) / (3 * t * t - 3 * t + 1);
+            }
+            /// <summary>
+            /// Realiza un suavizado cuártico de un valor.
+            /// </summary>
+            /// <returns>
+            /// Un valor correspondiente al suavizado aplicado.
+            /// </returns>
+            /// <param name="step">Número de paso a suavizar.</param>
+            /// <param name="total">Total de pasos.</param>
+            public static float Quartic(int step, int total)
+            {
+                float t = ((float)step / total);
+                return -((t - 1) * (t - 1) * (t - 1) * (t - 1)) + 1;
+            }
+        }
+        /// <summary>
         /// Comprueba si un número es primo.
         /// </summary>
         /// <returns>
