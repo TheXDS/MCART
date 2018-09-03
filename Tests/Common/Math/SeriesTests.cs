56d3b546847416618232a9be9b88fcb690bb3a5f
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2018-09-03 16:18:57 -0600

    Cambios de documentación

--- Tests/Common/Math/SeriesTests.cs
+++ Tests/Common/Math/SeriesTests.cs
@@ -1,11 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-using XUnit;
-
+using Xunit;
+using static TheXDS.MCART.Math.Series;
 namespace Common.Math
 {
-    class SeriesTests
+    public class SeriesTests
     {
+        [Fact]
+        public void FibonacciTest()
+        {
+            var a = Fibonacci().Take(16).ToArray();
+            var b = new long[]
+            {
+                0,
+                1,
+                1,
+                2,
+                3,
+                5,
+                8,
+                13,
+                21,
+                34,
+                55,
+                89,
+                144,
+                233,
+                377,
+                610
+            };
+            Assert.Equal(b,a);
+        }
     }
 }
