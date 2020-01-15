d5935d4ecc2b5ddeaaf0483a8166830e8753b77e
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2020-01-14 22:57:55 -0600

    Actualización de script de generación de documentación

--- src/publish-docs.ps1
+++ src/publish-docs.ps1
@@ -1,15 +1,20 @@
 ﻿#! /bin/pwsh
-docfx ..\docs\docfx.json
+try {
+    docfx ..\docs\docfx.json
+}
+catch {
+    Write-Error "No se pudo generar la documentación de ayuda." -CategoryReason "Parece que DocFx no está instalado." -Category NotInstalled
+}
 SOURCE_DIR = $PWD
 TEMP_REPO_DIR = $env:TMP\mcart-gh-pages
 if (!(Test-Path $TEMP_REPO_DIR)) {
     New-Item -Path $TEMP_REPO_DIR -ItemType "directory"
     git clone https://github.com/TheXDS/MCART.git --branch gh-pages $TEMP_REPO_DIR
 }
-cd $TEMP_REPO_DIR
+Set-Location $TEMP_REPO_DIR
 git rm -r *
 Copy-Item -Path $SOURCE_DIR/docs/_site/* -Destination . -Recurse -Force
 git add . -A
 git commit -m "Actualización de documentación"
 git push origin gh-pages
-cd $SOURCE_DIR
+Set-Location $SOURCE_DIR
