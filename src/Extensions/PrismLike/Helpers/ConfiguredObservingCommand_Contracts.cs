83f96dfbc88a28f199bda46de77ec9b5118d2a03
César Morgan <xds_xps_ivx@hotmail.com>
2021-12-19 17:44:16 -0600

    WIP: mejora de extensiones de ObservingCommand

---
+++ src/Extensions/PrismLike/Helpers/ConfiguredObservingCommand_Contracts.cs
@@ -0,0 +1,85 @@
+﻿/*
+ConfiguredObservingCommand_Contracts.cs
+
+This file is part of Morgan's CLR Advanced Runtime (MCART)
+
+Author(s):
+     César Andrés Morgan <xds_xps_ivx@hotmail.com>
+
+Copyright © 2011 - 2021 César Andrés Morgan
+
+Morgan's CLR Advanced Runtime (MCART) is free software: you can redistribute it
+and/or modify it under the terms of the GNU General Public License as published
+by the Free Software Foundation, either version 3 of the License, or (at your
+option) any later version.
+
+Morgan's CLR Advanced Runtime (MCART) is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General
+Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using TheXDS.MCART.Resources;
+using TheXDS.MCART.ViewModel;
+using static TheXDS.MCART.Helpers.ReflectionHelpers;
+using static TheXDS.MCART.Misc.Internals;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+using TheXDS.MCART.Resources;
+
+namespace TheXDS.MCART.Helpers
+{
+    public partial class ConfiguredObservingCommand<T> where T : INotifyPropertyChanged
+    {
+        //[Conditional("EnforceContracts")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [DebuggerNonUserCode]
+        private void IsBuilt_Contract()
+        {
+            if (IsBuilt) throw new InvalidOperationException();
+        }
+
+        [Conditional("EnforceContracts")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [DebuggerNonUserCode]
+        private void ListensToProperty_Contract<TValue>(Expression<Func<T, TValue>>[] properties)
+        {
+            IsBuilt_Contract();
+            if (!properties.Any()) throw Errors.EmptyCollection(properties);
+        }
+
+
+
+        [Conditional("EnforceContracts")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        [DebuggerNonUserCode]
+        private void ListensToCanExecute_Contract(MemberInfo member, Type t)
+        {
+            IsBuilt_Contract();
+            if (!GetAll<MemberInfo>(t).Contains(member))
+            {
+                throw Errors.MissingMember(t, member);
+            }
+        }
+
+        private static IEnumerable<TMember> GetAll<TMember>(Type? t) where TMember : MemberInfo
+        {
+            if (t is null || t == typeof(object)) return Array.Empty<TMember>();
+            return t.GetMembers().Concat(GetAll<TMember>(t.BaseType)).Concat(t.GetInterfaces().SelectMany(p => p.GetMembers())).OfType<TMember>();
+        }
+    }
+}
---
+++ src/Extensions/PrismLike/Helpers/ObservingCommandFactory.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Threading.Tasks;
+using TheXDS.MCART.Types.Extensions;
+using TheXDS.MCART.ViewModel;
+using TheXDS.MCART.Math;
+using static TheXDS.MCART.Helpers.ReflectionHelpers;
+
+namespace TheXDS.MCART.Helpers
+{
+    public partial class ConfiguredObservingCommand<T> where T : INotifyPropertyChanged
+    {
+        private readonly ObservingCommand command;
+        private readonly T observedObject;
+        private readonly List<Func<object?, bool>> canExecuteTree = new();
+
+        public bool IsBuilt { get; private set; }
+
+        internal ConfiguredObservingCommand(T observedObject, Action action)
+        {
+            command = new (this.observedObject = observedObject, action);
+        }
+
+        internal ConfiguredObservingCommand(T observedObject, Action<object?> action)
+        {
+            command = new(this.observedObject = observedObject, action);
+        }
+
+        internal ConfiguredObservingCommand(T observedObject, Func<Task> action)
+        {
+            command = new(this.observedObject = observedObject, action);
+        }
+
+        internal ConfiguredObservingCommand(T observedObject, Func<object?, Task> action)
+        {
+            command = new(this.observedObject = observedObject, action);
+        }
+
+        public ConfiguredObservingCommand<T> ListensTo(params Expression<Func<T, object?>>[] properties)
+        {
+            ListensTo<object?>(properties);
+            return this;
+        }
+
+        public ConfiguredObservingCommand<T> ListensTo<TValue>(params Expression<Func<T, TValue>>[] properties)
+        {
+            ListensToProperty_Contract(properties);
+            if (IsBuilt) throw new InvalidOperationException();
+            command.RegisterObservedProperty(properties.Select(GetProperty).Select(p => p.Name).ToArray());
+            return this;
+        }
+
+        public ConfiguredObservingCommand<T> ListensToCanExecute(Expression<Func<T, bool>> selector)
+        {
+            var pi = GetProperty(selector);
+            ListensToCanExecute_Contract(pi, typeof(T));
+            if (IsBuilt) throw new InvalidOperationException();
+            command.RegisterObservedProperty(pi.Name);
+            canExecuteTree.Add(_ => selector.Compile().Invoke(observedObject));
+            return this;
+        }
+
+        public ConfiguredObservingCommand<T> CanExecute(Func<object?, bool> canExecute)
+        {
+            IsBuilt_Contract();
+            canExecuteTree.Add(canExecute);
+            return this;
+        }
+
+        public ConfiguredObservingCommand<T> CanExecute(Func<bool> canExecute)
+        {
+            IsBuilt_Contract();
+            canExecuteTree.Add(_ => canExecute());
+            return this;
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfNotNull(params Expression<Func<T, object?>>[] properties)
+        {
+            return CanExecuteIf(p => p is not null, properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfNotDefault(params Expression<Func<T, object?>>[] properties)
+        {
+            return CanExecuteIf(p => p is not null && p != p.GetType().Default(), properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfFilled(params Expression<Func<T, string?>>[] properties)
+        {
+            return CanExecuteIf(p => !p.IsEmpty(), properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfValid(params Expression<Func<T, float>>[] properties)
+        {
+            return CanExecuteIf(p => p.IsValid(), properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfValid(params Expression<Func<T, float?>>[] properties)
+        {
+            return CanExecuteIf(p => p.HasValue && p.Value.IsValid(), properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfValid(params Expression<Func<T, double>>[] properties)
+        {
+            return CanExecuteIf(p => p.IsValid(), properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfValid(params Expression<Func<T, double?>>[] properties)
+        {
+            return CanExecuteIf(p => p.HasValue && p.Value.IsValid(), properties);
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfNotZero<TValue>(params Expression<Func<T, TValue>>[] properties) where TValue : notnull, IComparable<TValue>
+        {
+            return CanExecuteIf(p => p.CompareTo((TValue)p.GetType().Default()!) != 0, properties);
+        }
+
+        private ConfiguredObservingCommand<T> CanExecuteIf<TValue>(Func<TValue, bool> predicate, params Expression<Func<T, TValue>>[] properties)
+        {
+            if (IsBuilt) throw new InvalidOperationException();
+            ListensTo(properties);
+            CanExecute(_ => properties.Select(p => p.Compile().Invoke(observedObject)).All(predicate));
+            return this;
+        }
+
+        public ConfiguredObservingCommand<T> CanExecuteIfObservedIsFilled()
+        {
+            static bool IsFilled(object? p)
+            {
+                return p switch
+                {
+                    null => false,
+                    string s => !s.IsEmpty(),
+                    ValueType v => v != p.GetType().Default(),
+                    _ => true
+                };
+            }
+            if (IsBuilt) throw new InvalidOperationException();
+            var props = observedObject.GetType().GetProperties().Where(p => p.CanRead && p.CanWrite);
+            command.RegisterObservedProperty(props.Select(p => p.Name).ToArray());
+            canExecuteTree.Add(_ => props.Select(p => p.GetValue(observedObject)).All(IsFilled));
+            return this;
+        }
+
+        public ObservingCommand Build()
+        {
+            if (IsBuilt) return command;
+            IsBuilt = true;
+            return command.SetCanExecute(o => canExecuteTree.All(p => p(o)));            
+        }
+
+        public static implicit operator ObservingCommand (ConfiguredObservingCommand<T> command) => command.Build();
+    }
+
+    public static class ObservingCommandFactory
+    {
+        public static ConfiguredObservingCommand<T> CreateObservingCommand<T>(this T observedObject, Action action) where T : INotifyPropertyChanged
+        {
+            return new ConfiguredObservingCommand<T>(observedObject, action);
+        }
+        public static ConfiguredObservingCommand<T> CreateObservingCommand<T>(this T observedObject, Action<object?> action) where T : INotifyPropertyChanged
+        {
+            return new ConfiguredObservingCommand<T>(observedObject, action);
+        }
+        public static ConfiguredObservingCommand<T> CreateObservingCommand<T>(this T observedObject, Func<Task> action) where T : INotifyPropertyChanged
+        {
+            return new ConfiguredObservingCommand<T>(observedObject, action);
+        }
+        public static ConfiguredObservingCommand<T> CreateObservingCommand<T>(this T observedObject, Func<object?, Task> action) where T : INotifyPropertyChanged
+        {
+            return new ConfiguredObservingCommand<T>(observedObject, action);
+        }
+    }
+}
