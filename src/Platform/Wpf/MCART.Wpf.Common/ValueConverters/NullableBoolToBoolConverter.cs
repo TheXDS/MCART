a3a01bccccc753b559ec34422ada622dcaedb0b1
César Andrés Morgan <xds_xps_ivx@hotmail.com>
2023-09-24 12:23:44 -0600

    Nuevo ValueConverter

---
+++ src/Platform/Wpf/MCART.Wpf.Common/ValueConverters/NullableBoolToBoolConverter.cs
@@ -0,0 +1,53 @@
+﻿/*
+AllVisibleConverter.cs
+
+This file is part of Morgan's CLR Advanced Runtime (MCART)
+
+Author(s):
+     César Andrés Morgan <xds_xps_ivx@hotmail.com>
+
+Released under the MIT License (MIT)
+Copyright © 2011 - 2023 César Andrés Morgan
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+using System.Globalization;
+using TheXDS.MCART.ValueConverters.Base;
+
+namespace TheXDS.MCART.ValueConverters;
+
+/// <summary>
+/// Implementa un convertidor de valores que convierte objetos entre los tipos
+/// <c><see cref="bool"/>?</c> y <c><see cref="bool"/></c>.
+/// </summary>
+public class NullableBoolToBoolConverter : IValueConverter<bool?, bool>
+{
+    /// <inheritdoc/>
+    public bool Convert(bool? value, object? parameter, CultureInfo? culture)
+    {
+        return value ?? false;
+    }
+
+    /// <inheritdoc/>
+    public bool? ConvertBack(bool value, object? parameter, CultureInfo culture)
+    {
+        return value;
+    }
+}
